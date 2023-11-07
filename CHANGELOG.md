c20a1a7e0b0adc4188ed716ae4f84d386c3d1138
Ankith G <corra-337@Corra-337.local>
2023-11-07 14:38:07 +0530

    Updated the cli command file create-react-ank.js, and updated the documentation such as CHANGELOG.md, and  package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,3 +18,11 @@ and this project is to [CREATE A REACT APP](https://www.npmjs.com/package/create
 #### Changed
     - Added support for cloning the repository to a specified directory.
     - Added a help option to display usage information.
+
+### [1.0.2] - 2023-11-07
+
+#### Added
+    - Added interactive prompts for updating package.json fields.
+    - Supported keeping existing values in package.json if no input provided by the user.
+    - Enhanced user prompts by showing existing data in brackets for each field.
+
--- create-react-ank.js
+++ create-react-ank.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const { spawnSync } = require('child_process');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const repositoryUrl = 'https://github.com/ankithg03/react-18-boilerplate';
 
@@ -24,6 +28,38 @@ const cloneProcess = spawnSync('git', ['clone', repositoryUrl, destinationDirect
 
 if (cloneProcess.status === 0) {
   console.log('Repository cloned successfully.');
+
+  const packageJsonPath = `${destinationDirectory}/package.json`;
+
+  const fs = require('fs');
+
+  try {
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+    const updatePackageJson = (field, defaultValue) => {
+      const existingValue = packageJson[field] || defaultValue;
+      readline.question(`Enter a value for ${field} in package.json (or press Enter to keep the existing value: [${existingValue}]): `, (input) => {
+        if (input.trim() !== '') {
+          packageJson[field] = input;
+        } else {
+          packageJson[field] = defaultValue;
+        }
+
+        if (field === 'repository') {
+          fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+          console.log('Package.json updated with user-provided values.');
+          readline.close();
+        } else {
+          updatePackageJson('name', packageJson.name);
+        }
+      });
+    };
+
+    updatePackageJson('name', packageJson.name);
+  } catch (error) {
+    console.error('Error reading or updating package.json:', error);
+  }
 } else {
   console.error('Error cloning repository.');
+  process.exit(1);
 }
